8b665313e10830a374b5c6c7efed2f769a7e2acf
Elaa Affes <elaa.affes@supcom.tn>
2026-05-13 03:18:15 +0100

    Update Multiplication_4bit.vhd

--- Multiplication_4bit.vhd
+++ Multiplication_4bit.vhd
@@ -1,24 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 30.01.2026 08:54:11
--- Design Name: 
--- Module Name: Multiplication_4bit - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
