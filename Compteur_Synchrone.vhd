512da8ca98139e5f2ca650af6cc2b823054256b0
Elaa Affes <elaa.affes@supcom.tn>
2026-05-13 03:15:48 +0100

    Update Compteur_Synchrone.vhd

--- Compteur_Synchrone.vhd
+++ Compteur_Synchrone.vhd
@@ -1,24 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 30.01.2026 08:56:40
--- Design Name: 
--- Module Name: Compteur_Synchrone - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
