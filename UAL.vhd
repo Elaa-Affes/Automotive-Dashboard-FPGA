93073aca5c97d2e899d75585b094fedd95f57e18
Elaa Affes <elaa.affes@supcom.tn>
2026-05-13 03:19:22 +0100

    Update UAL.vhd

--- UAL.vhd
+++ UAL.vhd
@@ -1,24 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 08.02.2026 22:06:40
--- Design Name: 
--- Module Name: UAL - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
